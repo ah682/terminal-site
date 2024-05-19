8a7d397b9baa787f305fadaf2e7f6b037a345a38
ah682 <96323387+ah682@users.noreply.github.com>
2024-05-20 00:33:39 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# terminal-site
+# ah682.github.io/terminal-site
+
+# View site at: https://ah682.github.io/terminal-site
+
+# Developed with JavaScript, Tailwind, HTML5
---
+++ js/commands.js
@@ -0,0 +1,150 @@
+var twitter = "";
+var linkedin = "https://www.linkedin.com/in/ava-hall-35297a254/";
+var instagram = "";
+var github = "https://github.com/ah682";
+var hackerrank = "";
+var sudo = "";
+var email = "mailto:avahall929@gmail.com";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>I'm Ava.</span>",
+  "<br>",
+  "<li>🔭 I’m currently a student at the University of Central Lancashire</li>",
+  "<li>🌱 My field of intetest is</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Fullstack Development</pre>`,
+  `<li><pre class="whitespace-pre-wrap">👯 I would love to work with anyone who wants to build a 
+   professional website for their business or even a personal 
+   website to showcase their portfolio.
+   I love learning new stuff and using it to improve my skills.
+   I use languages such as Dart, Flutter, JavaScript, React, CSS, Tailwind and Python when building my 
+   projects.
+   You can learn more about the projects that I have built using 
+   the project command.
+   You can browse around my website to find out more about 
+   me.
+   *Try using a social command.`,
+  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check my github profile to view my projects
+   *Use the github command to take a look at my GitHub profile.`,
+  `<li>📫 Contact me at <a href= "mailto:avahall929@gmail.com" target="_blank">avahall929@gmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'Twitter        <a href="' +
+    twitter +
+    '" target="blank">twitter.com/ah682' +
+    "</a>",
+  'Linkedin      <a href="' +
+    linkedin +
+    '" target="blank">https://www.linkedin.com/in/ava-hall-35297a254/' +
+    "</a>",
+  'Instagram  <a href="' +
+    instagram +
+    '" target="blank">instagram.com/ah682/' +
+    "</a>",
+  'Github         <a href="' +
+    github +
+    '" target="blank">https://github.com/ah682' +
+    "</a>",
+  "<br>",
+];
+
+expertise = [
+  "<br>",
+  `<pre class="whitespace-pre-wrap skills">
+  - C++</pre>`,
+  `<pre class="whitespace-pre-wrap skills">
+  - Java</pre>`,
+  `<pre class="whitespace-pre-wrap skills">
+  - HTML5</pre>`,
+  `<pre class="whitespace-pre-wrap skills">
+  - Tailwind CSS</pre>`,
+  `<pre class="whitespace-pre-wrap skills">
+  - React</pre>`,
+  `<pre class="whitespace-pre-wrap skills">
+  - Git(Versions Control)</pre>`,
+  `<pre class="whitespace-pre-wrap skills">
+  - Linux</pre>`,
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  `<a href="https://github.com/ah682/react-calculator" target="blank"><span class='underline'>React Calculator</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">Calculator developed with React.js
+
+Project is live at <a href="https://github.com/ah682/react-calculator" target="blank">https://github.com/ah682/react-calculator/</a>
+
+Built With
+
+    *React.js
+    *CSS
+    *HTML5
+`,
+
+  "<br>",
+
+  `<a href="https://github.com/ah682/word-filter" target="blank"><span class='underline'>Word Filter</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap"> Filters/censors certain words target="blank"><span class='underline'>
+
+Built With
+
+    *C++
+`,
+
+  "<br>",
+
+  `<a href="https://github.com/ah682/hide-error" target="blank"><span class='underline'>Hide Error</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">Removes Error Text and Sounds from World of Warcraft Client - Lua with World of Warcraft API
+
+Built With
+
+    *Lua
+
+    "<br>",
+
+    
+`,
+
+  `<a href="https://github.com/ah682/quote-generator" target="blank"><span class='underline'>Quote Generator</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">Generates an assortment of quotes
+
+Built With
+
+  *React JS
+  *CSS
+  *HTML5
+
+  "<br>",
+`,
+];
+
+help = [
+  `<pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am.
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">skills</span>     
+   <span class="command">↳</span>&nbsp;Skills on tech.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;Let's colaborate.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome To My Portfolio — Type <span class="command text-[#75e1e7]">help</span> For A List Of Supported Commands.</span></div>`,
+];
