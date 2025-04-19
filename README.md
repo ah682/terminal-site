130f5e1057a641d1af9e236af6c56e7e26938014
ah682 <96323387+ah682@users.noreply.github.com>
2025-04-19 18:54:45 +0100

    Initial commit: Terminal website with Tailwind CSS

---
+++ README.md
@@ -0,0 +1,5 @@
+# ah682.github.io/terminal-site
+
+# View site at: https://ah682.github.io/terminal-site
+
+# Developed with JavaScript, Tailwind, HTML5
---
+++ js/commands.js
@@ -0,0 +1,31 @@
+var github = "https://github.com/ah682";
+var sudo = "";
+
+aboutme = [
+  "<br>",
+  "Software Developer in the UK specialising in various technologies and programming languages.",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  '<a href="https://github.com/ah682" target="blank">github.com/ah682</a>',
+  "<br>"
+];
+
+help = [
+  `<pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome To My Portfolio — Type <span class="command text-[#75e1e7]">help</span> For A List Of Supported Commands.</span></div>`,
+];
